d26fa1f556ba739ad867b94a89e70878e5367292
Josh <spartan139@yahoo.com>
2025-12-05 16:51:11 -0800

    Update vite.config.ts
    Updated repo

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: './', // change to '/<repo>/' for GitHub Pages
+  base: '/minecraft_slice_planner/',
   resolve: { alias: { '@': '/src' } }
 })
